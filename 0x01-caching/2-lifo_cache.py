bce6adced58ffca9d7933e9957b5fe48e0c620d2
Raymond Lukwago <lukwagoraymond3@gmail.com>
2023-05-17 23:50:45 +0300

    Corrected

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -12,28 +12,18 @@ class LIFOCache(BaseCaching):
     policy"""
 
     def __init__(self):
-        self.ordered_cache_data = OrderedDict()
+        self.stack = list()
         super().__init__()
 
     def put(self, key, item):
         if key and item:
-            if len(self.ordered_cache_data) == BaseCaching.MAX_ITEMS:
-                if key not in self.ordered_cache_data:
-                    discarded_dict = self.ordered_cache_data.popitem(last=True)
-                    print(f'DISCARD: {discarded_dict[0]}')
-                    self.ordered_cache_data.update({key: item})
-                elif key in self.ordered_cache_data:
-                    print(f'DISCARD: {key}')
-                    self.ordered_cache_data.update({key: item})
-                stored_keys = [keys for keys in self.ordered_cache_data.keys()]
-                self.cache_data.clear()
-                for keys in stored_keys:
-                    self.cache_data.update(
-                        {keys: self.ordered_cache_data[keys]})
+            if len(self.cache_data) == BaseCaching.MAX_ITEMS and key not in self.stack:
+                discarded_dict = self.stack.pop()
+                del self.cache_data[discarded_dict]
+                print(f'DISCARD: {discarded_dict}')
 
-            elif len(self.cache_data) < 5:
-                self.cache_data.update({key: item})
-                self.ordered_cache_data.update({key: item})
+            self.cache_data.update({key: item})
+            self.stack.append(key)
 
     def get(self, key):
         """Returns a value of a particular key from
